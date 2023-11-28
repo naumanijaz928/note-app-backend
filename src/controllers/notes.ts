ca2f6869c1bbc71738dd751232a7895e784c1324
Numan Ijaz <naumanijaz928@gmail.com>
2023-11-28 21:57:25 +0500

    Notes crud completed

--- src/controllers/notes.ts
+++ src/controllers/notes.ts
@@ -1,7 +1,9 @@
 import { RequestHandler } from "express";
 import NoteModel from "../models/note";
 import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
+/*  --------------- GET ALL NOTE --------------- */
 export const getNotes: RequestHandler = async (req, res, next) => {
   try {
     const notes = await NoteModel.find().exec();
@@ -13,11 +15,17 @@ export const getNotes: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
-
+/*  --------------- GET Single NOTE --------------- */
 export const getNote: RequestHandler = async (req, res, next) => {
   const noteId = req.params.noteId;
   try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid note id");
+    }
     const note = await NoteModel.findById(noteId).exec();
+    if (!note) {
+      throw createHttpError(404, "Note not found");
+    }
     res.status(200).json({
       message: "ok",
       note,
@@ -31,7 +39,7 @@ interface CreateNoteBody {
   title: string;
   text?: string;
 }
-
+/*  --------------- Create Single NOTE --------------- */
 export const createNote: RequestHandler<
   unknown,
   unknown,
@@ -49,6 +57,72 @@ export const createNote: RequestHandler<
     });
 
     res.status(201).json(newNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface UpdateNoteParams {
+  noteId: string;
+}
+
+interface UpdateNoteBody {
+  title?: string;
+  text?: string;
+}
+/*  --------------- UPDATE Single NOTE --------------- */
+export const updateNote: RequestHandler<
+  UpdateNoteParams,
+  unknown,
+  UpdateNoteBody,
+  unknown
+> = async (req, res, next) => {
+  const noteId = req.params.noteId;
+  const newTitle = req.body.title;
+  const newText = req.body.text;
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid note id");
+    }
+    if (!newTitle) {
+      throw createHttpError(400, "Note must have a title");
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "note not found");
+    }
+    note.title = newTitle;
+    note.text = newText;
+    const updatedNote = await note.save();
+    res.status(200).json({
+      message: "ok",
+      note: updatedNote,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/*  --------------- DELETE Single NOTE --------------- */
+export const deleteNote: RequestHandler = async (req, res, next) => {
+  const noteId = req.params.noteId;
+
+  try {
+    if (!mongoose.isValidObjectId(noteId)) {
+      throw createHttpError(400, "Invalid note id");
+    }
+
+    const note = await NoteModel.findById(noteId).exec();
+
+    if (!note) {
+      throw createHttpError(404, "Note not found");
+    }
+
+    await note.deleteOne();
+
+    res.sendStatus(204);
   } catch (error) {
     next(error);
   }
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -6,4 +6,6 @@ const router = express.Router();
 router.get("/", NotesController.getNotes);
 router.get("/:noteId", NotesController.getNote);
 router.post("/", NotesController.createNote);
+router.patch("/:noteId", NotesController.updateNote);
+router.delete("/:noteId", NotesController.deleteNote);
 export default router;
